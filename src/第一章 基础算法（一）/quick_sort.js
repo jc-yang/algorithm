ef259675182b778024fa3a01aab010b7aea5043c
yangjc <843401905@qq.com>
2021-09-16 17:45:29 +0800

    quick sort

--- src/第一章 基础算法（一）/quick_sort.js
+++ src/第一章 基础算法（一）/quick_sort.js
@@ -28,10 +28,26 @@ function quick_sort(q, l, r) {
     do i++; while (q[i] < x);
     do j--; while (q[j] > x);
     if (i < j) swap(q, i, j)
-  }
-  quick_sort(q, l, j)
+  } // 循环交换完后，x左侧都比x小，右侧都比x大，之后分别排序左右两个区间
+  quick_sort(q, l, j) // 思考下为什么是 j
   quick_sort(q, j + 1, r)
 }
+
+/**
+ * 思考为什么是 j：
+ * i 和 j 均可以用于分割区间，只是需要完全对称，如：
+ *   quick_sort(q, l, j) 
+ *   quick_sort(q, j + 1, r)
+ * 或者
+ *   quick_sort(q, l, i - 1) 
+ *   quick_sort(q, i, r)
+ * 此时须特别注意边界问题，以 j 分割时，起点 x=q[l]不会有问题
+ * 但如果以 i 分割，起点 x=q[l] 会引起数组越界进入死循环，改为 x=q[r] 即可
+ * 或者将起点设置为中心 x=q[Math.round((l + r) / 2)]，则不会涉及到边界问题
+ * 
+ * 为什么进入死循环：
+ * 以数组[1, 2]为例，x=1 循环完毕进入递归后, 会一直在区间[0, 1]中递归，进入死循环
+ */
 
 function swap(q, i, j) {
   let temp = q[i]
