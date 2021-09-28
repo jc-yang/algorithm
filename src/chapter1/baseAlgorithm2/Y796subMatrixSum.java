5e955dc6892037cf64699bb599a4cb8f8f7a4858
yangjc <843401905@qq.com>
2021-09-28 15:45:56 +0800

    sub matrix sum

--- src/chapter1/baseAlgorithm2/Y796subMatrixSum.java
+++ src/chapter1/baseAlgorithm2/Y796subMatrixSum.java
@@ -41,18 +41,29 @@ public class Y796subMatrixSum {
         int n = in.nextInt();
         int m = in.nextInt();
         int q = in.nextInt();
-        int[][] arr = new int[n + 1][m + 1];
+
+        int[][] a = new int[n + 1][m + 1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                arr[i][j] = in.nextInt();
+                a[i][j] = in.nextInt();
             }
         }
-        // 输出查看 arr
-        for (int i = 1; i <= n; i++) {
+
+        int[][] s = new int[n + 1][m + 1];
+        for(int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                System.out.print(arr[i][j] + " ");
+                s[i][j] = s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1] + a[i][j];
             }
-            System.out.println();
         }
+
+        while (q-- > 0) {
+            int x1 = in.nextInt();
+            int y1 = in.nextInt();
+            int x2 = in.nextInt();
+            int y2 = in.nextInt();
+            int res = s[x2][y2] - s[x1 - 1][y2] - s[x2][y1 - 1] + s[x1 - 1][y1 - 1];
+            System.out.println(res);
+        }
+
     }
 }
