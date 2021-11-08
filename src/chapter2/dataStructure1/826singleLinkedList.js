24cfecbd2ece2feb5271c663cce9c2a57ede9e23
yangjc <843401905@qq.com>
2021-11-08 21:47:12 +0800

    single linked list

--- src/chapter2/dataStructure1/826singleLinkedList.js
+++ src/chapter2/dataStructure1/826singleLinkedList.js
@@ -43,4 +43,66 @@ const N = 100010
 // head 表示头结点的下标
 // e[i] 表示节点 i 的值
 // ne[i] 表示节点 i 的下一个节点的下标
+// idx 存储当前已经用到了哪个点（下标）
 let head, e = new Array(N).fill(0), ne = new Array(N).fill(0), idx
+
+// 初始化
+function init(params) {
+  head = -1
+  idx = 0
+}
+
+// 将x插入到头结点
+function add_to_head(x) {
+  e[idx] = x // 存当前节点
+  ne[idx] = head // 当前节点next指向原链表的head （栓新绳）
+  head = idx // head变为当前节点的下标 （解旧绳）
+  idx++  // 指向下一节点
+}
+
+// 将x节点插入到下标为k的节点的后面
+function add(k, x) {
+  e[idx] = x
+  ne[idx] = ne[k] // 当前节点的next指向原链表k指向的next
+  ne[k] = idx // 原链表k指向的next指向当前节点
+  idx++
+}
+
+// 将下标k后面的节点删除
+function remove(k) {
+  ne[k] = ne[ne[k]]
+}
+
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.output
+})
+let inputCount = 0, m
+init()
+rl.on('line', line => {
+  inputCount++
+  if(inputCount === 1) m = parseInt(line)
+  else if (inputCount <= m + 1) {
+    const lll = line.split(' ')
+    const op = lll[0]
+    if (op === 'H') {
+      const x = parseInt(lll[1])
+      add_to_head(x)
+    } else if (op === 'D') {
+      const k = parseInt(lll[1])
+      remove(k)
+    } else {
+      const k = parseInt(lll[1])
+      const x = parseInt(lll[2])
+      add(k, x)
+    }
+  }
+})
+rl.on('close', () => {
+  let res = ''
+  for(let i = head; i !== -1; i = ne[i]) {
+    res += e[i] + ' '
+  }
+  console.log(res)
+})
